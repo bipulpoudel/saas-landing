c43cb29b599fb3c3abfbd81b6ff9ee3693dae2e5
bipulpoudel <bipulpoudel55555@gmail.com>
2024-06-13 14:37:14 +1000

    [added] SEO and meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,42 @@ const fontSans = FontSans({
 });
 
 export const metadata: Metadata = {
-  title: "SaaS Landing Page",
-  description: "Beautiful landing page for your SaaS product",
+  title: "SaaS Landing page",
+  description:
+    "Free, open-source SaaS landing page template to help you quickly build a beautiful website for your SaaS product.",
+  openGraph: {
+    title: "Free and Open-Source SaaS Landing Page",
+    description:
+      "Free, open-source SaaS landing page template to help you quickly build a beautiful website for your SaaS product.",
+    type: "website",
+    url: "https://saas-landing-bice.vercel.app",
+    locale: "en_US",
+    siteName: "Free and Open-Source SaaS Landing Page",
+    images: [
+      {
+        url: "https://saas-landing-bice.vercel.app/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Free and Open-Source SaaS Landing Page",
+        type: "image/jpg",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Free and Open-Source SaaS Landing Page",
+    description:
+      "Free, open-source SaaS landing page template to help you quickly build a beautiful website for your SaaS product.",
+    images: [
+      {
+        url: "https://saas-landing-bice.vercel.app/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Free and Open-Source SaaS Landing Page",
+        type: "image/jpg",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
